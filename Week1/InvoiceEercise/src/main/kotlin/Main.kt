8f4861d6406f9d085969b7cb40bcbaedb7f52d60
crendo11 <crendo11@eafit.edu.co>
2022-04-26 13:57:04 -0500

    verion 2 of invoice solution

--- Week1/InvoiceEercise/src/main/kotlin/Main.kt
+++ Week1/InvoiceEercise/src/main/kotlin/Main.kt
@@ -1,31 +1,88 @@
+import com.sun.org.apache.xpath.internal.operations.Bool
+
 fun main(){
     // create a list of products
-    var product1 = Product("Camisa", 150000F);
-    var product2 = Product("Pantalon", 120000F);
-    var product3 = Product("Zapatos", 250000F);
+    var product1 = Product("Camisa", 150000F, true)
+    var product2 = Product("Pantalon", 120000F, true)
+    var product3 = Product("Zapatos", 250000F, true)
+    // create list of discounts
+    var discount1 = Discount(0.2F, "WELCOME")
+    var discount2 = Discount(0.1F, "FIRSTBUY")
 
-    val products = arrayOf<Product>(product1,product2,product3);
+    // Create lists
+    val products = mutableListOf<Product>(product1,product2,product3)
+    val discounts = mutableListOf<Discount>(discount1, discount2)
 
-    var totalValue: Float = 0F;
-    // calculate total value
-    products.forEach{
-        totalValue += it.value;
-    }
+    val invoice = Invoice(products, discounts)
 
-    // pint total price
-    println("El costo total de los productos es: $${totalValue}");
-
-    //calc discount
-    println("Introduce un porcentaje de descuento: ")
-    var discount = readLine()!!.toFloat();
-    discount /= 100;
-    println("El descuento ingresado fue del: ${discount * 100}%")
-    // apply discount
-    var payingPrice: Float = totalValue * (1 - discount);
-    println("El costo total es: $${payingPrice}")
+    displayInvoice(invoice)
 }
 
-class Product(name: String, value: Float){
+class Product(name: String, value: Float, includesIva: Boolean){
     val name: String = name;
     val value: Float = value;
+    val includesIva: Boolean = includesIva;
+}
+
+class Invoice (var products: MutableList<Product> = mutableListOf<Product>(),
+               var discounts: MutableList<Discount> = mutableListOf<Discount>()
+    ){
+    // Invoice internal variables
+    var total: Float = 0F
+    var totalDiscount: Float = 0F
+    var totalWithDiscount: Float = 0F
+
+    init{
+        calcVariables()
+    }
+    // Methods for updating invoice variables
+    fun calcVariables() {
+        // call methods to update the variables
+        calcTotal()
+        calcTotalDiscounts()
+        calculateTotalWithDiscount()
+    }
+    fun calcTotal() {
+        // Calculate total value without any discount
+        products.forEach { product ->
+            total += product.value
+        }
+    }
+    fun calcTotalDiscounts() {
+        // calculate total amount of discounts
+        var discountCount: Float = 0F
+        discounts.forEach{discount ->
+            discountCount += discount.value
+        }
+        totalDiscount = total * discountCount
+    }
+    fun calculateTotalWithDiscount() {
+        totalWithDiscount = total - totalDiscount;
+    }
+    // Methods to add discounts and products to an invoice
+    fun addProduct(product: Product){
+        products.add(product)
+        // update variables
+        calcVariables()
+    }
+
+    fun addDiscount(discount: Discount) {
+        discounts.add(discount)
+        // update variables
+        calcVariables()
+    }
+
+
+}
+class Discount (value: Float, code: String){
+    // value goes from 0 to 1
+    var value: Float = value;
+    var code: String = code;
+}
+
+fun displayInvoice(invoice: Invoice){
+    // dsiplay total value
+    println("El valor total es:    $${invoice.total}")
+    println("Decuentos:            $${invoice.totalDiscount}")
+    println("Total a pagar:        $${invoice.totalWithDiscount}")
 }
