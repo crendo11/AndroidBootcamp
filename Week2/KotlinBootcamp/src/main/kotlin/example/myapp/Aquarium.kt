f7c98611ad9d96862feaf7d556d826a47e4ad28e
crendo11 <crendo11@eafit.edu.co>
2022-05-01 21:21:55 -0500

    class visibility and inheritance

--- Week2/KotlinBootcamp/src/main/kotlin/example/myapp/Aquarium.kt
+++ Week2/KotlinBootcamp/src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,31 +1,42 @@
 package example.myapp
+import java.lang.Math.PI
 // Add class constructor (implemented in the definition of the class)
-class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40){
-    // crerating getter for volume
-    var volume: Int
-        get() = width * height * length / 1000  // 1000 cm^3 = 1 l
-        // setter
+open class Aquarium (open var length: Int = 100, open var width: Int = 20, open var height: Int = 40){
+    // create open variable for volume
+    open var volume: Int
+        get() = width * height * length / 1000
         set(value) {
             height = (value * 1000) / (width * length)
         }
-    // secondary constructor
-    constructor(numberOfFish: Int): this(){
-        // 2,000 cm^3 per fish + extra room so water doesn't spill
-        val tank = numberOfFish * 2000 * 1.1
-        // calculate the height needed
-        height = (tank / (length * width)).toInt()
-        println("Scondary constructor activated")
-    }
-    // adding init blocks
-    init {
-        println("aquarium initializing")
-    }
-    // methods for the class
+
+    // deifne variable for shape
+    open val shape = "rectangle"
+
+    //Add an open water property with a getter that returns 90% of the volume of the Aquarium
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
+    // funtion to return the the shape and the amount of voume occupied
     fun printSize() {
+        println(shape)
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
         // 1 l = 1000 cm^3
-        println("Volume: $volume l")
+        println("Volume: $volume l Water: $water l (${water/volume*100.0}% full)")
     }
+}
+
+class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+    // ellipse area = pi * r1 * r2
+        get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        // set the value of the area given a volume
+        set(value) {
+            height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+        }
+    // override itnernal variables
+    override var water = volume * 0.8
+    override val shape = "cylinder"
+
 }
--- Week2/KotlinBootcamp/src/main/kotlin/example/myapp/main.kt
+++ Week2/KotlinBootcamp/src/main/kotlin/example/myapp/main.kt
@@ -1,29 +1,11 @@
 package example.myapp
 
 fun buildAquarium() {
-    val myAquarium = Aquarium()
-    // call method
-    myAquarium.printSize()
-    // modifi properties
-    myAquarium.height = 60
-    myAquarium.printSize()
-    // default height and length
-    val aquarium2 = Aquarium(width = 25)
-    aquarium2.printSize()
-    // default width
-    val aquarium3 = Aquarium(height = 35, length = 110)
-    aquarium3.printSize()
-    // everything custom
-    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
-    aquarium4.printSize()
-    val aquarium5 = Aquarium(numberOfFish = 29)
-    // call to create an Aquarium using your new secondary constructor. Print the size and volume.
-    val aquarium6 = Aquarium(numberOfFish = 29)
-    aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
-    // now it is not necessary to calculate volume
-    // println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} l")
+    val myaquarium = Aquarium(25, 25, 40)
+    myaquarium.printSize()
+    // create a towertank
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
 
 fun main() {
