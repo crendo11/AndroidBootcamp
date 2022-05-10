70f6e68c280c0e1c5935e4c409c72dd9c8e87474
crendo11 <crendo11@eafit.edu.co>
2022-05-10 10:08:27 -0500

    using resources

--- Week3/AndroidTestApp/app/src/main/java/com/example/androidtestapplication/MainActivity.kt
+++ Week3/AndroidTestApp/app/src/main/java/com/example/androidtestapplication/MainActivity.kt
@@ -4,39 +4,41 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 
+// declare
+
 class MainActivity : AppCompatActivity() {
     // onCreated is called when the application is launched
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // this toast is not shown
-        Toast.makeText(this,"Hello create", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, getString(R.string.hello_create), Toast.LENGTH_SHORT).show()
     }
 
     //
     override fun onStart(){
         super.onStart()
-        Toast.makeText(this,"Hello start", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,getString(R.string.hello_start), Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
         super.onResume()
-        Toast.makeText(this,"Hello resume", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,getString(R.string.hello_resume), Toast.LENGTH_SHORT).show()
     }
 
     override fun onPause() {
         super.onPause()
-        Toast.makeText(this,"Hello pause", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,getString(R.string.hello_pause), Toast.LENGTH_SHORT).show()
     }
 
     override fun onStop() {
         super.onStop()
-        Toast.makeText(this,"Hello stop", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,getString(R.string.hello_stop), Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Toast.makeText(this,"Hello destroy", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,getString(R.string.hello_destroy), Toast.LENGTH_SHORT).show()
     }
 
 }
